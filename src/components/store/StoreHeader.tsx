01623de14c7ff3ba5751a6d32bdb2c1e99c74d99
ameeralig <ameer_a16@icloud.com>
2025-11-20 17:30:05 +0300

    Update StoreHeader.tsx

--- src/components/store/StoreHeader.tsx
+++ src/components/store/StoreHeader.tsx
@@ -125,7 +125,7 @@ const StoreHeader = ({ storeName, colorTheme, fontSettings }: StoreHeaderProps) 
 
   return storeName ? (
     <h1 
-      className={`text-8xl md:text-10xl font-bold text-center mb-0 ${isCustomColor ? '' : themeColors}`}
+      className={`text-6xl md:text-10xl font-bold text-center mb-0 ${isCustomColor ? '' : themeColors}`}
       style={getStoreNameStyle()}
       dir="rtl"
     >
